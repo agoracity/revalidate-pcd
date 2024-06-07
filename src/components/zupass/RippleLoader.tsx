f188f7dc644f54d98d0de1fdc02d23c1f467abed
Matias <marazza.matias@gmail.com>
2024-06-07 17:05:11 -0300

    RippleLoader

--- src/components/zupass/RippleLoader.tsx
+++ src/components/zupass/RippleLoader.tsx
@@ -1,11 +1,13 @@
 export const RippleLoader = () => {
     return (
-      <div className="loaderWrapper">
+  <div className="w-screen h-screen flex justify-center items-center bg-zupass-bg-dark-primary">
+        <div className="loaderWrapper">
         <div className="loader">
           <div></div>
           <div></div>
         </div>
       </div>
+  </div>
     );
   };
   
